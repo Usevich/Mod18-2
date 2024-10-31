1bff5c2cd4b9b82f86352d5e5a42529b6a80078e
Usevich <tisiztomsk@gmail.com>
2024-10-31 19:32:28 +0700

    Домашка модуль 18 - 2

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,14 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+    # Функциональное представление
+def function_based_view(request):
+    return render(request, 'second_task/func_template.html')
+
+    # Классовое представление
+class ClassBasedView(View):
+    def get(self, request):
+        return render(request, 'second_task/class_template.html')
+    
